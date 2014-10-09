0851aa1d278a39177dde8de9ca981e94ef62f2f1
Simon BASLE <simon.basle@gmail.com>
2014-10-09 16:27:06 +0200

    added ls and nicemount alias

--- alias/alias.plugin.zsh
+++ alias/alias.plugin.zsh
@@ -2,3 +2,11 @@
 alias topdirs='du -h | sort -hr | head'
 alias topfiles='du -axh . | sort -nr | head'
 alias move='rsync -ahP --remove-source-files'
+
+#ls related
+alias ls='ls --color'
+alias l='ls -lh'
+alias ll='ls -la'
+
+#nicer presentation of mount
+function nicemount() { (echo "DEVICE PATH TYPE FLAGS" && mount | awk '$2="";1') | column -t ; }
