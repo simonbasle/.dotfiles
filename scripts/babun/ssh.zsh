c055db6ce4d901c7daf722ea7e67a5f8e8484f06
Simon Baslé <simon.basle@gmail.com>
2014-10-13 14:05:56 +0200

    ensure git ssh don't use putty

--- scripts/babun/ssh.zsh
+++ scripts/babun/ssh.zsh
@@ -1,8 +1,11 @@
+#ensure we don't delegate git ssh handling to PuTTY
+unset GIT_SSH
+
 SSHAGENT=/usr/bin/ssh-agent
 SSHAGENTARGS="-s"
 if [ -z "$SSH_AUTH_SOCK" -a -x "$SSHAGENT" ]; then
     eval `$SSHAGENT $SSHAGENTARGS`
     trap "kill $SSH_AGENT_PID" 0
 else
-    echo "ssh agent not started"
+    echo "ssh agent not started, maybe clear SSH_AUTH_SOCK"
 fi
