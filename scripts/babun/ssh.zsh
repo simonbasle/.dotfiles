cb346cd7768ae5cece925f487afa7c6b30fe2435
Simon Baslé <simon.basle@gmail.com>
2014-10-13 19:30:35 +0200

    make ssh-agent persistent between terms

--- scripts/babun/ssh.zsh
+++ scripts/babun/ssh.zsh
@@ -1,11 +1,19 @@
 #ensure we don't delegate git ssh handling to PuTTY
 unset GIT_SSH
 
-SSHAGENT=/usr/bin/ssh-agent
-SSHAGENTARGS="-s"
-if [ -z "$SSH_AUTH_SOCK" -a -x "$SSHAGENT" ]; then
-    eval `$SSHAGENT $SSHAGENTARGS`
-    trap "kill $SSH_AGENT_PID" 0
-else
-    echo "ssh agent not started, maybe clear SSH_AUTH_SOCK"
+# If no SSH agent is already running, start one now. Re-use sockets so we never
+# have to start more than one session.
+
+ssh-add -l >/dev/null 2>&1
+if [ $? = 2 ]; then
+   # No ssh-agent running
+   rm -rf $SSH_AUTH_SOCK
+   # force to reuse same AUTH SOCKET
+   export SSH_AUTH_SOCK=~/.ssh-socket
+   # >| allows output redirection to over-write files if no clobber is set
+   ssh-agent -a $SSH_AUTH_SOCK >| /tmp/.ssh-script
+   source /tmp/.ssh-script
+   echo $SSH_AGENT_PID >| ~/.ssh-agent-pid
+   rm /tmp/.ssh-script
+   echo "ssh agent running as PID $SSH_AGENT_PID"
 fi
