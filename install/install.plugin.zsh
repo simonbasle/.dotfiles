ccef9109f2bd48203598fd23359efaaaedc6ac67
Simon Baslé <simon.basle@gmail.com>
2014-10-17 12:06:01 +0200

    fix conditional check

--- install/install.plugin.zsh
+++ install/install.plugin.zsh
@@ -7,7 +7,7 @@ function dotinstall() {
 	echo "Attempting to install $1"
 	WHERE=$PWD
 	FROM=$DOTFILES/install/$1
-	if [[ -d "$FROM" -a -x "$FROM/install.zsh" ]]; then
+	if [[ -x "$FROM/install.zsh" ]]; then
 		source "$FROM/install.zsh" "$FROM" "$WHERE"
 	else
 		echo "no $FROM directory or directory does not contain an executable install.zsh"
