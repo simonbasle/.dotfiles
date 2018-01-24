16c71650276d8d1ef35197ef9221c84383d8548d
Simon Baslé <sbasle@pivotal.io>
2018-01-24 12:55:34 +0100

    add iTerm2 utilities support on osx

--- scripts/osx/init.zsh
+++ scripts/osx/init.zsh
@@ -7,3 +7,7 @@ export JAVA_HOME=$(/usr/libexec/java_home -v 1.8)
 
 #used to source homebrew github access token HOMEBREW_GITHUB_API_TOKEN
 #previous committed one was revoked, moved to uncomitted ../common/private.zsh
+
+#iTerm2 utilities with command line integration
+# see https://iterm2.com/documentation-utilities.html
+test -e "${HOME}/.iterm2_shell_integration.zsh" && source "${HOME}/.iterm2_shell_integration.zsh"
