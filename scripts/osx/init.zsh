c35f5fae0eae991742d62db8136849f477eac74b
Simon Baslé <sbasle@vmware.com>
2020-11-25 12:22:06 +0100

    Add fix for Atom not getting focus from CLI on osx
     - Added a function acting as an alias for `atom`
    - Git core.editor cannot use the function/alias, so its content was
      instead directly duplicated in the git config
    
    The principle is to use `open -a atom` to force opening/focusing the
    Atom window, THEN perform the open file command (with --wait in the
    case of the git editor).

--- scripts/osx/init.zsh
+++ scripts/osx/init.zsh
@@ -5,3 +5,9 @@ echo "OSX init.zsh"
 #iTerm2 utilities with command line integration
 # see https://iterm2.com/documentation-utilities.html
 test -e "${HOME}/.iterm2_shell_integration.zsh" && source "${HOME}/.iterm2_shell_integration.zsh"
+
+#workaround on OSX atom not focusing when launched from command line + use it for git editor
+function atom() {
+	open -a atom && /usr/local/bin/atom $@
+}
+git config --global core.editor 'open -a atom && atom --wait'
