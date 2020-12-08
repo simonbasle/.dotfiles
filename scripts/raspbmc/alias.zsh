2c7f5b725bf98ceabff8aa0d0aed172461efa5b2
Simon Baslé <sbasle@vmware.com>
2020-12-08 16:29:05 +0100

    Rename scripts named init.zsh or with same name as directory
    This is because such naming patterns cause Antigen to ONLY load said
    scripts instead of all the script files in the directory.

--- scripts/raspbmc/alias.zsh
+++
@@ -1,3 +0,0 @@
-#--progress is unsupported on raspbmc's rsync
-alias move='rsync -ahP --remove-source-files '
-
--- scripts/raspbmc/init.zsh
+++
@@ -1,7 +0,0 @@
-#taken most important parts of original raspbmc's .bashrc and /etc/profile
-
-PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/local/games:/usr/games:/opt/vc/bin:/home/pi/.xbmc-current/xbmc-bin/bin"
-RPI_UPDATE_UNSUPPORTED=1
-export PATH
-export RPI_UPDATE_UNSUPPORTED
-
