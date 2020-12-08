b33265723aec25b66203108c095580cb72c5a350
Simon Baslé <sbasle@vmware.com>
2020-12-08 16:31:41 +0100

    rename jenv.zsh to java.zsh, select Java 8 on startup

---
+++ scripts/osx/java.zsh
@@ -0,0 +1,13 @@
+#jenv uses brew's folder
+export JENV_ROOT=/usr/local/opt/jenv
+
+#jenv autocompletion
+if which jenv > /dev/null; then eval "$(jenv init -)"; fi
+
+#alternative ways to switch to different default versions of java
+alias j14="export JAVA_HOME=`/usr/libexec/java_home -v 12`; java -version"
+alias j11="export JAVA_HOME=`/usr/libexec/java_home -v 11`; java -version"
+alias j8="export JAVA_HOME=`/usr/libexec/java_home -v 1.8`; java -version"
+
+echo "Use j8/j11/j14 to select default java version through JAVA_HOME. Will now default to Java 8"
+export JAVA_HOME=`/usr/libexec/java_home -v 1.8`
--- scripts/osx/jenv.zsh
+++
@@ -1,5 +0,0 @@
-#jenv uses brew's folder
-export JENV_ROOT=/usr/local/opt/jenv
-
-#jenv autocompletion
-if which jenv > /dev/null; then eval "$(jenv init -)"; fi
