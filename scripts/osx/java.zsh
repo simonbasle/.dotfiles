ec6020b8b622fa3fd91fa81c751af62920e05037
Simon Baslé <simon.basle@gmail.com>
2021-10-07 13:40:50 +0200

    Add j17 to java versions for osx

--- scripts/osx/java.zsh
+++ scripts/osx/java.zsh
@@ -5,9 +5,9 @@ export JENV_ROOT=/usr/local/opt/jenv
 if which jenv > /dev/null; then eval "$(jenv init -)"; fi
 
 #alternative ways to switch to different default versions of java
-alias j14="export JAVA_HOME=`/usr/libexec/java_home -v 12`; java -version"
-alias j11="export JAVA_HOME=`/usr/libexec/java_home -v 11`; java -version"
 alias j8="export JAVA_HOME=`/usr/libexec/java_home -v 1.8`; java -version"
+alias j11="export JAVA_HOME=`/usr/libexec/java_home -v 11`; java -version"
+alias j17="export JAVA_HOME=`/usr/libexec/java_home -v 17`; java -version"
 
-echo "Use j8/j11/j14 to select default java version through JAVA_HOME. Will now default to Java 8"
+echo "Use j8/j11/j17 to select default java version through JAVA_HOME. Will now default to Java 8"
 export JAVA_HOME=`/usr/libexec/java_home -v 1.8`
