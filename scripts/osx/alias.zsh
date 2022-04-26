ebd0d020783c1888a7936e78b45638cf4bdfdc10
Simon Baslé <simon.basle@gmail.com>
2022-04-26 15:39:26 +0200

    Add broot initialization and alias tree to broot

--- scripts/osx/alias.zsh
+++ scripts/osx/alias.zsh
@@ -5,9 +5,14 @@
 # see https://iterm2.com/documentation-utilities.html
 test -e "${HOME}/.iterm2_shell_integration.zsh" && source "${HOME}/.iterm2_shell_integration.zsh"
 
+#broot
+source /Users/sbasle/.config/broot/launcher/bash/br
+
 #workaround on OSX atom not focusing when launched from command line + use it for git editor
 function atom() {
 	open -a atom && /usr/local/bin/atom $@
 }
 git config --global core.editor 'open -a atom && atom --wait'
 #gh config set editor "atom --wait"
+
+alias tree=br
